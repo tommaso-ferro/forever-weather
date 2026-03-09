98821f5dba4f824c71d0dacef45815a44ab4992f
Ferro Tommaso <tommaso.ferro@cz.lan>
2026-03-09 09:49:54 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const searchBar = document.querySelector('#search-bar');
 const searchButton = document.querySelector('#search-button');
+const weatherInfoText = document.querySelector('#weather-info-h1');
+const highTempText = document.querySelector('#high-temp');
+const lowTempText = document.querySelector('#low-temp');
+const positionText = document.querySelector('#position-text');
+const dateText = document.querySelector('#date');
+const humidityText = document.querySelector('#humidity');
+const windSpeedText = document.querySelector('#wind-speed');
 
 async function fetchWeather(city) {
 
@@ -11,13 +18,25 @@ async function fetchWeather(city) {
     console.log(weatherCondition);
     updateBackground(weatherCondition);
 
+    const weatherDescription = data.weather[0].description;
+    weatherInfoText.textContent = weatherDescription;
+
     const temperature = data.main.temp;
     const tempMin = data.main.temp_min;
     const tempMax = data.main.temp_max;
     const humidity = data.main.humidity;
+    humidityText.textContent = `${humidity}%`;
+
     const windSpeed = data.wind.speed;
+    windSpeedText.textContent = windSpeed;
+
+    const cityText = data.name;
+    const countryText = data.sys.country;
+    updatePosition(cityText, countryText);
 
     document.querySelector('#degrees').textContent = `${Math.round(temperature)}°`;
+    highTempText.textContent = `H: ${Math.round(tempMax)}°`;
+    lowTempText.textContent = `L: ${Math.round(tempMin)}°`;
 }
     
 function updateBackground(weatherCondition) {
@@ -54,10 +73,24 @@ function updateBackground(weatherCondition) {
 
 searchButton.addEventListener('click', () => {
     const city = searchBar.value;
+    const country = data.sys.country;
+    const city2 = data.name;
     if (city) {
         fetchWeather(city);
+        updatePosition(city2, country);
     }
 });
 
+function updatePosition(city, country) {
+    positionText.textContent = `${city}, ${country}`;
+}
 
-window.addEventListener('load', fetchWeather);
+function dateUpdate(){
+    let now = new Date();  
+    dateText.textContent = `(${now.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' })})`;
+}
+
+window.addEventListener('load', () => {
+    fetchWeather();
+    dateUpdate();
+});
